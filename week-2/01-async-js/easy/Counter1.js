cfd1151aa6093d99005131573d74afe2f15c244d
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-12-11 18:45:37 +0530

    working on easy

---
+++ week-2/01-async-js/easy/Counter1.js
@@ -0,0 +1,6 @@
+let a=1;
+setInterval(()=>{
+    console.clear();
+    console.log(a);
+    a++;
+},1000)
---
+++ week-2/01-async-js/easy/Counter2.js
@@ -0,0 +1,8 @@
+let n=5
+for (let i = 1; i <=5; i++) {
+    setTimeout(()=>{
+        console.clear();
+        console.log(i);
+    },i*1000)
+    
+}
